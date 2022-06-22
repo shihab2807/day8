2b0bd935dcfa558a678734871184947a0da71aa5
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 07:29:03 +0100

    day8 done

--- day8/src/day8/demo1.java
+++ day8/src/day8/demo1.java
@@ -1,10 +1,42 @@
 package day8;
 
-public class demo1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+package com.ank.statickeyword;
 
-	}
+class method {
+	int rollno;
+	String name;
+	static 
+	String college= "Acharya";
 
-}
+
+	 method (int r, String n) {
+rollno = r;
+name = n;
+	 }
+	 static void change() {
+		 college= "AIT";
+		
+	 }
+	 void display() {
+			System.out.println(rollno+ " "+ name+ " "+ college);
+		}
+
+		
+
+			public static void main(String[] args) {
+				change();
+				variable s1 = new variable (101,"Abc",college);
+				variable s2 = new variable (101,"Ade",college);
+				variable s3 = new variable (101,"Afg",college);
+				variable s4 = new variable (101,"Ahi",college);
+				
+				s1.display();
+				s2.display();
+				s3.display();
+				s4.display();
+
+			}
+
+		}
